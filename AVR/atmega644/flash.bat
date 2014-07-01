86ac417988f7a5eb733b88e8fb01ba70c8af8b70
heise <cm@ix.de>
2014-07-01 18:43:37 +0200

    new

---
+++ AVR/atmega644/flash.bat
@@ -0,0 +1,2 @@
+rem Flashes ATmega644P with DIAMEX PROG-S on serial port [Param1]
+avrdude -C "avrdude.conf" -pm644 -cstk500v2 -P COM%1 -B4 -U flash:w:"GRBL.HEX":i -U lfuse:w:0xf7:m -U hfuse:w:0xd9:m -U efuse:w:0xfd:m 
